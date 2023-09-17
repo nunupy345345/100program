7dddfb73d1c9a7a3807538a8bb13cc5aff7b542e
nunupy345345 <airinota@outlook.jp>
2023-09-17 19:26:39 +0900

    変更5

--- src/pages/coupon.js
+++ src/pages/coupon.js
@@ -1,14 +1,14 @@
 import {NavLink,Rocation,useParams,useLocation} from "react-router-dom";
 import React, { useState, useEffect} from 'react' ;
-import './start.css';
+import './coupon.css';
 import norenImage from "../images/header.png";
-
+import midoriWaku from "../images/midoriWaku.png";
+import yen20 from "../images/20yen.png";
 
 export const Coupon  = () => {
   const handleClick1 = () => {
     window.location.href = "/start";
   }
-
    //ページを変えても値を受け渡すやつ
    const search = useLocation().search;
 
@@ -20,13 +20,70 @@ export const Coupon  = () => {
    const receivedData1 = queryParams.get('var1');
    const receivedData2 = queryParams.get('var2');
    const receivedData3 = queryParams.get('time');
+   
+  
+   let splitList = receivedData1.split(',');
+   
   return(
     <div>
-    <h1>クーポン取得画面</h1>
-    <div>{receivedData1}</div>
-      <div>missCounted: {receivedData2}</div>
-      <div>経過時間: {receivedData3} 秒</div>
-    <button className="startButton" onClick={() => {handleClick1()}} id="hai">startへ</button> 
+      <header className='header'><img src={norenImage} onClick={() => handleClick1()}/></header>
+      <body>
+      <div className="receipt">お買い物レシート</div>
+      <div className="list0">1.{splitList[0]}</div>
+      <div className="list">
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+        <div>2.{splitList[1]}</div>
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+        <div>3.{splitList[2]}</div>
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+        <div>4.{splitList[3]}</div>
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+        <div>5.{splitList[4]}</div>
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+        <div>6.{splitList[5]}</div>
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+        <div>7.{splitList[6]}</div>
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+        <div>8.{splitList[7]}</div>
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+        <div>9.{splitList[8]}</div>
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+        <div>10.{splitList[9]}</div>
+        <div className="explain">
+          <div><img src={midoriWaku}/></div>
+          <div><img src={yen20}/></div>
+        </div>
+      </div>
+      </body>
+      <div className="missCount">missCounted: {receivedData2}</div>
+      <div className="time">経過時間: {receivedData3} 秒</div>
     </div>
   )
 }
